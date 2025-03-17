a07b151ed332848a41f946797523311351b6c134
Mzdudul <mzzdev.e@gmail.com>
2025-03-17 16:26:12 -0300

    Explorando condiçoes com while, do while

---
+++ src/conta_banco/explorantowhile/Match.java
@@ -0,0 +1,10 @@
+package conta_banco.explorantowhile;
+
+public class Match {
+
+    public static While format() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'format'");
+    }
+
+}
---
+++ src/conta_banco/explorantowhile/Telefonema.java
@@ -0,0 +1,23 @@
+package conta_banco.explorantowhile;
+
+import java.util.Random;
+
+
+
+public class Telefonema {
+    public static void main(String[] args) {
+        System.out.println("Discando");
+
+        do{
+            System.out.println("Telefone tocando");
+        }while(tocando());
+        System.out.println("Alo");
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3) ==1;
+        System.out.println("Atendeu? " + atendeu);
+
+        return ! atendeu;
+    }
+}
