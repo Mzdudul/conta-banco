7b9a43474373ab0094bfeb781bf01bc8f4dd4646
Mzdudul <mzzdev.e@gmail.com>
2025-03-17 15:41:37 -0300

    explorando for em arrays

--- src/explorandofor/For.java
+++ src/explorandofor/For.java
@@ -2,8 +2,14 @@ package explorandofor;
 
 public class For {
     public static void main(String[] args) {
-        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
-            System.out.println("Contando carneirinhos " + carneirinhos);
+        // for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+        //     System.out.println("Contando carneirinhos " + carneirinhos);
+        // }
+
+        String alunos [] = {"Eduardo", "Jonas", "Julia", "Marcos" };
+
+        for(int x = 0; x < alunos.length; x++){
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
         }
     }
 }
