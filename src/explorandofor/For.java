b3ca1d803e4d3266e83e119c1c21f01a40979ad9
Mzdudul <mzzdev.e@gmail.com>
2025-03-17 15:44:22 -0300

    um pouco sobre for each

--- src/explorandofor/For.java
+++ src/explorandofor/For.java
@@ -8,8 +8,12 @@ public class For {
 
         String alunos [] = {"Eduardo", "Jonas", "Julia", "Marcos" };
 
-        for(int x = 0; x < alunos.length; x++){
-            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        // for(int x = 0; x < alunos.length; x++){
+        //     System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        // }
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
         }
     }
 }
