54c655039b16484c0dbcf13e5c9e7f0226bcabfc
Mzdudul <mzzdev.e@gmail.com>
2025-03-18 10:02:08 -0300

    explorando exeçoes com try catch

--- src/conta_banco/explorantowhile/Telefonema.java
+++ src/conta_banco/explorantowhile/Telefonema.java
@@ -6,18 +6,24 @@ import java.util.Random;
 
 public class Telefonema {
     public static void main(String[] args) {
-        System.out.println("Discando");
+    //     System.out.println("Discando");
 
-        do{
-            System.out.println("Telefone tocando");
-        }while(tocando());
-        System.out.println("Alo");
-    }
+    //     do{
+    //         System.out.println("Telefone tocando");
+    //     }while(tocando());
+    //     System.out.println("Alo");
+    // }
 
-    private static boolean tocando(){
-        boolean atendeu = new Random().nextInt(3) ==1;
-        System.out.println("Atendeu? " + atendeu);
+    // private static boolean tocando(){
+    //     boolean atendeu = new Random().nextInt(3) ==1;
+    //     System.out.println("Atendeu? " + atendeu);
 
-        return ! atendeu;
-    }
+    //     return ! atendeu;
+        int numero = 1;
+
+        for (int x=1; x<2; x++){
+            numero = numero + x;
+        }
+        System.out.println("o numero e: " + numero);
+}
 }
